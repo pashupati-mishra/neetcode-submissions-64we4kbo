2cbd69dedec9eba1e07d79dab7f4a8ca052f1d51
pashupati-mishra <pashupatiacet05a5@gmail.com>
2026-04-09 20:15:35 +0530

    Add: reverse-bits - submission-0

---
+++ Data Structures & Algorithms/reverse-bits/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t ans=0;
+        for(int i=0;i<32;i++)
+        {
+            if(1&(n>>i))
+            {
+                ans=ans+1;
+            }
+            if(i<31)ans=ans<<1;
+        }
+        return ans;
+    }
+};
